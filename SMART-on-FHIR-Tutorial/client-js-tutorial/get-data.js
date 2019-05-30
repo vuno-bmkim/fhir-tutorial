0e963248b2b047f7aa6604bce1c35ba1c1db068c
vuno-bmkim <bmkim@vuno.co>
2019-05-30 13:53:02 +0900

    tutorial code for fhir

--- SMART-on-FHIR-Tutorial/client-js-tutorial/get-data.js
+++ SMART-on-FHIR-Tutorial/client-js-tutorial/get-data.js
@@ -28,11 +28,13 @@ function getPatientName (pt) {
   }
   
   // Create a FHIR client (server URL, patient id in `demo`)
-  var smart = FHIR.client(demo),
-      pt = smart.patient;
-  
+  var smart = FHIR.client(demo);
+  var pt = smart.patient;
+  console.log(smart);
+  //console.log(pt);
   // Create a patient banner by fetching + rendering demographics
   smart.patient.read().then(function(pt) {
+    //console.log(pt);
     displayPatient (pt);
   });
   
@@ -47,3 +49,25 @@ function getPatientName (pt) {
           }
      });
   });
+
+
+
+  smart.api.read({type: 'MedicationOrder', id: 'smart-MedicationOrder-127'}).done(function(results){
+    console.log('read');
+    console.log(results);
+  })
+
+  smart.api.vread({type: 'MedicationOrder', id: 'smart-MedicationOrder-127', versionId: 1}).done(function(results){
+    console.log('vread');
+    console.log(results);
+  })
+
+  smart.api.search({type: 'MedicationOrder'}).done(function(results){
+    console.log('search');
+    console.log(results);
+  })
+
+  smart.patient.api.search({type: 'MedicationOrder'}).done(function(results){
+    console.log('patient search');
+    console.log(results);
+  })
